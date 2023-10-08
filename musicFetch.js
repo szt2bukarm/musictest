4329c21db959c0c76514f42b56a29f330802667f
Bukvity Ármin <107731846+szt2bukarm@users.noreply.github.com>
2023-10-08 18:36:51 +0200

    Add files via upload

--- musicFetch.js
+++ musicFetch.js
@@ -22,10 +22,59 @@ const getData = async function(artist) {
     }
     });
     const data = await res.json()
+
     for (let i = 0; i < 10; i++) {
         artists.push(data.tracks.items[i].artists[0].name + " - " + data.tracks.items[i].name);
     }
     return artists
+}
+
+const newReleaseseContainer = document.querySelector('.new-releases-container')
+const getNewReleases = async function() {
+    let placeholder =`
+        <div class="item-card">
+        <img class="item-image-placeholder">
+        <div class="item-desc-placeholder">
+            <div class="item-title-placeholder"></div>
+            <p class="item-artist-placeholder"></p>
+            <div class="item-info">
+                <div class="item-type-placeholder"></div>
+                <div class="item-trackcount-placeholder"></div>
+            </div>
+        </div>
+    </div>`
+
+    for (let i = 0; i < 6; i++) {
+        newReleaseseContainer.insertAdjacentHTML('beforeend',placeholder)
+    }
+
+    const accessToken = await getToken()
+    const res = await 
+    fetch('https://api.spotify.com/v1/browse/new-releases', {
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        }
+    });
+    const data = await res.json()
+    newReleaseseContainer.innerHTML = ""
+    
+    for (let i = 0; i < 6; i++) {
+        let html = `
+            <div class="item-card">
+                <img class="item-image" src="${data.albums.items[i].images[1].url}" alt="" srcset="">
+                <div class="item-desc">
+                    <p class="item-title">${(data.albums.items[i].name).length > 15 ? (data.albums.items[i].name).substring(0,15) + "..." : data.albums.items[i].name}</p>
+                    <p class="item-artist">${data.albums.items[i].artists[0].name}</p>
+                    <div class="item-info">
+                        <div class="item-type">${data.albums.items[i].album_type}</div>
+                        <div class="item-trackcount">${data.albums.items[i].total_tracks + " tracks"}</div>
+                    </div>
+                </div>
+            </div>
+        `
+        
+        newReleaseseContainer.insertAdjacentHTML('beforeend',html)
+    }
 }
 
 
@@ -43,3 +92,5 @@ search.addEventListener('click',async function() {
         results.insertAdjacentHTML('beforeend',html)
     });
 })
+
+// getNewReleases()
